ddf326dba25360f7217ae70dc228f9d6b9df44ef
keymoki <mloyygraduate7@gmail.com>
2024-03-28 22:32:28 +0800

    css bug fixing, About page added, burger menu added

--- ohmyschapp.py
+++ ohmyschapp.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def homepage():
     return flask.render_template('homepage.html', name=__name__)
 
+@app.route('/about/')
+def about():
+    return flask.render_template('about.html', name=__name__)
+
 if __name__ == '__main__':
     app.run(debug=True)
